290aa0a74e3a6b8f38887e7c8fea6c657cd77a7e
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-02-12 22:35:40 +0800

    section and year level

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
+use App\Models\Section;
 use App\Models\User;
+use App\Models\YearLevel;
+use Illuminate\Support\Facades\Cache;
 
 class UserController extends Controller
 {
     public function index()
     {
         $search = request()->input('search');
+
         $users = User::with(['media'])
             ->when($search, function ($query) use ($search) {
                 $query->where('name', 'like', '%'.$search.'%');
@@ -20,6 +24,7 @@ class UserController extends Controller
         return inertia('User/Index', [
             'users' => UserResource::collection($users),
             'search' => $search,
+            'props' => $this->props(),
         ]);
     }
 
@@ -27,5 +32,21 @@ class UserController extends Controller
     {
 
         return inertia('User/Show', ['user' => new UserResource($user->load('media'))]);
+    }
+
+    private function props()
+    {
+
+        $props = Cache::remember('sections_year', now()->addHours(24), function () {
+            $sections = Section::select(['id', 'name'])->get();
+            $year_levels = YearLevel::select(['id', 'name'])->get();
+
+            return [
+                'sections' => $sections,
+                'year_levels' => $year_levels,
+            ];
+        });
+
+        return $props;
     }
 }
